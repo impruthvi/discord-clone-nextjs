b5bedd0989fe844d2a9dff1ea6588518e9d8cfe7
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-10-12 11:16:33 +0530

    init commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Open_Sans } from 'next/font/google'
+
+const font = Open_Sans({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Discord Clone',
+  description: 'Discord Clone created by @impruthvi',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
