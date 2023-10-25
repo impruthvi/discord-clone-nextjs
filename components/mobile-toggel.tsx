9fa4ce75f83f642b057d3d62e8f3548149a75e6a
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-10-25 12:56:39 +0530

    Implemented Chat Header Component

---
+++ components/mobile-toggel.tsx
@@ -0,0 +1,28 @@
+import { Menu } from "lucide-react";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import NavigationSidebar from "@/components/navigation/navigation-sidebar";
+import ServerSidebar from "@/components/server/server-sidebar";
+
+interface MobileToggleProps {
+  serverId: string;
+}
+
+export const MobileToggle: React.FC<MobileToggleProps> = ({ serverId }) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 flex gap-0">
+        <div className="w-[72px]">
+          <NavigationSidebar />
+        </div>
+        <ServerSidebar serverId={serverId} />
+      </SheetContent>
+    </Sheet>
+  );
+};
