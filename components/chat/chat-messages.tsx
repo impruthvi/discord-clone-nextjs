2fc461f718785e4cc1058bd463f9e043e98b1a3a
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-10-26 14:20:20 +0530

    Implemented Chat Message Functionality

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,7 +1,16 @@
 "use client";
 
-import { Member } from "@prisma/client";
+import { Member, Message, Profile } from "@prisma/client";
 import { ChatWelcome } from "./chat-welcome";
+import { Loader2, ServerCrash } from "lucide-react";
+import { useChatQuery } from "@/hooks/use-chat-query";
+import { Fragment } from "react";
+
+type MessageWithMemberWithProfile = Message & {
+  member: Member & {
+    profile: Profile
+  };
+}
 
 interface ChatMessagesProps {
   name: string;
@@ -26,10 +35,52 @@ export const ChatMessages: React.FC<ChatMessagesProps> = ({
   paramValue,
   type,
 }) => {
+  const queryKey = `chat:${chatId}`;
+
+  const { data, status, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useChatQuery({
+      queryKey,
+      apiUrl,
+      paramKey,
+      paramValue,
+    });
+  if (status === "loading") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <Loader2 className="w-7 h-7 text-zinc-500 animate-spin my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Loading messages...
+        </p>
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <ServerCrash className="w-7 h-7 text-zinc-500 my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Something went wrong!
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex-1 flex flex-col py-4 overflow-y-auto">
       <div className="flex-1" />
       <ChatWelcome type={type} name={name} />
+      <div className="flex flex-col-reverse mt-auto">
+        {data?.pages.map((page, i) => (
+          <Fragment key={i}>
+            {page.items.map((message:MessageWithMemberWithProfile) => (
+              <div className="" key={message.id}>
+                  {message.content}
+              </div>
+            ))}
+          </Fragment>
+        ))}
+      </div>
     </div>
   );
 };
