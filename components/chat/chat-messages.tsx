d32607458beffaa7c4299dc4146530885e3caab4
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-10-26 12:23:31 +0530

    Implemented Welcome message and chat message component

---
+++ components/chat/chat-messages.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Member } from "@prisma/client";
+import { ChatWelcome } from "./chat-welcome";
+
+interface ChatMessagesProps {
+  name: string;
+  member: Member;
+  chatId: string;
+  apiUrl: string;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
+  paramKey: "conversationId" | "channelId";
+  paramValue: string;
+  type: "conversation" | "channel";
+}
+
+export const ChatMessages: React.FC<ChatMessagesProps> = ({
+  name,
+  member,
+  chatId,
+  apiUrl,
+  socketUrl,
+  socketQuery,
+  paramKey,
+  paramValue,
+  type,
+}) => {
+  return (
+    <div className="flex-1 flex flex-col py-4 overflow-y-auto">
+      <div className="flex-1" />
+      <ChatWelcome type={type} name={name} />
+    </div>
+  );
+};
